00f85535be4ec2850bec7eba90cd010cc73a53d1
HeKaiYu <hekaiyu_301@163.com>
2022-04-16 16:10:47 +0800

    chore: scheduler

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -24,11 +24,38 @@ describe("effect", () => {
     const runner = effect(() => {
       foo++
       return 'foo'
-    })
+    }, {})
 
     expect(foo).toBe(11)
     const r = runner()
     expect(foo).toBe(12)
     expect(r).toBe('foo')
+  })
+
+  it('scheduler', () => {
+    let dummy
+    let run: any
+    const scheduler = jest.fn(() => {
+      run = runner
+    })
+
+    const obj = reactive({ foo: 1 })
+    const runner = effect(() => {
+      dummy = obj.foo
+    }, {
+      scheduler
+    })
+
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1)
+    // should be called on first trigger
+    obj.foo++
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    // should not run yet
+    expect(dummy).toBe(1)
+    // manually run
+    run()
+    // should have run
+    expect(dummy).toBe(2)
   })
 })
