05d16d3e6e6b5a2563ef7e0e41cb819f68dd9b3d
HeKaiYu <hekaiyu_301@163.com>
2022-05-03 22:18:18 +0800

    chore: 20.实现初始化component主流程

---
+++ src/runtime-core/component.ts
@@ -0,0 +1,49 @@
+
+export function createComponentInstance(vnode) {
+  const component = {
+    vnode,
+    type: vnode.type,
+  }
+  return component
+}
+
+
+export function setupComponent(instance) {
+
+  // TODO: 
+  // initProps()
+  // initSlots()
+
+
+  setupStateComponent(instance);
+}
+
+function setupStateComponent(instance: any) {
+  const component = instance.type;
+
+  const { setup } = component
+
+  if (setup) {
+    const setupResult = setup()
+
+    handleSetupResult(instance, setupResult)
+  }
+}
+
+function handleSetupResult(instance, setupResult) {
+
+  if (typeof setupResult === 'object') {
+    instance.setupState = setupResult
+  }
+
+  finishComponentSetup(instance)
+}
+
+function finishComponentSetup(instance) {
+  const component = instance.type
+
+
+  if (component.render) {
+    instance.render = component.render
+  }
+}
