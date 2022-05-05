fb784a6d190a1faa1fb13330afbe76bbe7930293
HeKaiYu <hekaiyu_301@163.com>
2022-05-05 21:32:56 +0800

    chore: 27.实现组件emit功能

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -1,4 +1,5 @@
 import { shallowReadonly } from "../reactivity/reactive";
+import { emit } from "./componentEmit";
 import { initProps } from "./componentProps";
 import { PublicInstanceProxyHandlers } from "./componentPublicInstance";
 
@@ -6,7 +7,13 @@ export function createComponentInstance(vnode) {
   const component = {
     vnode,
     type: vnode.type,
+    setupState: {},
+    props: {},
+    emit: () => { }
   }
+
+  component.emit = emit.bind(null, component) as any
+
   return component
 }
 
@@ -29,7 +36,9 @@ function setupStateComponent(instance: any) {
   instance.proxy = new Proxy({ _: instance }, PublicInstanceProxyHandlers)
 
   if (setup) {
-    const setupResult = setup(shallowReadonly(instance.props))
+    const setupResult = setup(shallowReadonly(instance.props), {
+      emit: instance.emit,
+    })
 
     handleSetupResult(instance, setupResult)
   }
