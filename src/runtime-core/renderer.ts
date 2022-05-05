75f64cb2796a9fd7081e4625371241cf8a4df53d
HeKaiYu <hekaiyu_301@163.com>
2022-05-05 18:05:33 +0800

    chore: 24.实现shapeFlags

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { ShapeFlags } from '../shared/ShapeFlags';
 import { isObject } from './../shared/index';
 import { createComponentInstance, setupComponent } from "./component";
 
@@ -8,10 +9,10 @@ export function render(vnode, container) {
 
 function patch(vnode, container) {
 
-  // TODO:处理element
-  if (typeof vnode.type === 'string') {
+  const { shapeFlag } = vnode
+  if (shapeFlag & ShapeFlags.ELEMENT) {
     processElement(vnode, container)
-  } else if (isObject(vnode.type)) {
+  } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
     processComponent(vnode, container);
   }
 
@@ -24,12 +25,12 @@ function processElement(vnode: any, container: any) {
 function mountElement(vnode: any, container: any) {
   const el = (vnode.el = document.createElement(vnode.type));
 
-  const { children, props } = vnode
+  const { children, props, shapeFlag } = vnode
 
-  if (typeof children === 'string') {
+  if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
     el.textContent = children
 
-  } else if (Array.isArray(children)) {
+  } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
     mountChildren(vnode, el)
   }
 
