d56964546a2a23ff2811dc3704e68c6b127e7568
HeKaiYu <hekaiyu_301@163.com>
2022-05-11 15:16:12 +0800

    chore: 44. 实现解析element标签

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,11 @@
 import { NodeTypes } from "./ast"
 
+
+const enum TagType {
+  Start,
+  End
+}
+
 export function baseParse(content: string) {
 
   const context = createParserContext(content)
@@ -12,8 +18,13 @@ function parseChildren(context) {
   const nodes: any = []
 
   let node
-  if (context.source.startsWith('{{')) {
+  const s = context.source
+  if (s.startsWith('{{')) {
     node = parseInterpolation(context)
+  } else if (s.startsWith('<')) {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context)
+    }
   }
 
   nodes.push(node)
@@ -49,6 +60,29 @@ function parseInterpolation(context) {
 }
 
 
+function parseElement(context) {
+
+  const element = parseTag(context, TagType.Start)
+
+  parseTag(context, TagType.End)
+  return element
+}
+
+
+function parseTag(context: any, type: TagType) {
+  const match: any = /^<\/?([a-z]+)/.exec(context.source)
+  const tag = match[1]
+
+  advanceBy(context, match[0].length)
+  advanceBy(context, 1)
+
+  if (type === TagType.End) return
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag
+  }
+}
 
 function advanceBy(context: any, length: number) {
   context.source = context.source.slice(length)
