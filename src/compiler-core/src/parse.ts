f795601f9b74e47fff3ecb7ff68a88c8e57f6818
HeKaiYu <hekaiyu_301@163.com>
2022-05-12 11:43:28 +0800

    chore: 46. 实现解析三种联合类型

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -10,34 +10,76 @@ export function baseParse(content: string) {
 
   const context = createParserContext(content)
 
-  return createRoot(parseChildren(context))
+  return createRoot(parseChildren(context, []))
 
 }
 
-function parseChildren(context) {
+function parseChildren(context, ancestors) {
   const nodes: any = []
 
-  let node
-  const s = context.source
-  if (s.startsWith('{{')) {
-    node = parseInterpolation(context)
-  } else if (s.startsWith('<')) {
-    if (/[a-z]/i.test(s[1])) {
-      node = parseElement(context)
+  while (!isEnd(context, ancestors)) {
+
+    let node
+    const s = context.source
+    if (s.startsWith('{{')) {
+      node = parseInterpolation(context)
+    } else if (s.startsWith('<')) {
+      if (/[a-z]/i.test(s[1])) {
+        node = parseElement(context, ancestors)
+      }
     }
-  }
 
-  if (!node) {
-    node = parseText(context)
-  }
+    if (!node) {
+      node = parseText(context)
+    }
 
-  nodes.push(node)
+    nodes.push(node)
+
+  }
 
   return nodes
 }
 
+function isEnd(context: any, ancestors) {
+  const s = context.source
+
+  if (s.startsWith('</')) {
+    for (let i = ancestors.length - 1; i >= 0; i--) {
+      const tag = ancestors[i].tag;
+      if (startsWithEndTagOpen(s, tag)) {
+        return true
+      }
+    }
+  }
+
+  // if (parentTag && s.startsWith(`</${parentTag}>`)) {
+  //   return true
+  // }
+
+  return !s
+}
+
 function parseText(context: any) {
-  const content = parseTextData(context, context.source.length)
+
+  let endIndex = context.source.length
+  let endToken = ['<', '{{']
+
+  for (let i = 0; i < endToken.length; i++) {
+    const index = context.source.indexOf(endToken[i]);
+    if (index !== -1 && endIndex > index) {
+      endIndex = index
+    }
+
+  }
+
+
+  const index = context.source.indexOf(endToken)
+
+  if (index !== -1) {
+    endIndex = index
+  }
+
+  const content = parseTextData(context, endIndex)
 
   advanceBy(context, content.length)
 
@@ -80,14 +122,24 @@ function parseInterpolation(context) {
 }
 
 
-function parseElement(context) {
+function parseElement(context, ancestors) {
 
-  const element = parseTag(context, TagType.Start)
+  const element: any = parseTag(context, TagType.Start)
+  ancestors.push(element)
+  element.children = parseChildren(context, ancestors)
+  ancestors.pop()
 
-  parseTag(context, TagType.End)
+  if (startsWithEndTagOpen(context.source, element.tag)) {
+    parseTag(context, TagType.End)
+  } else {
+    throw new Error(`缺少结束标签:${element.tag}`)
+  }
   return element
 }
 
+function startsWithEndTagOpen(source, tag) {
+  return source.startsWith('</') && source.slice(2, 2 + tag.length).toLowerCase() === tag.toLowerCase()
+}
 
 function parseTag(context: any, type: TagType) {
   const match: any = /^<\/?([a-z]+)/.exec(context.source)
