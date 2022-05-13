2abbe1c0c3d04754abfcdd73ec0526c4ff2cae54
HeKaiYu <hekaiyu_301@163.com>
2022-05-13 16:06:13 +0800

    chore: 51.实现代码生成三种联合类型

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -6,6 +6,8 @@ export const isObject = (val) => {
   return val !== null && typeof val === 'object'
 }
 
+export const isString = (value) => typeof value === 'string'
+
 // Object.is() 方法判断两个值是否为同一个值。
 export const hasChanged = (val, newVal) => {
   return !Object.is(val, newVal)
